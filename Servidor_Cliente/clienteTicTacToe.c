9054c52a551fb3a58366f95efb2248b5694a1f1b
PalaciosRolando <65567672+PalaciosRolando@users.noreply.github.com>
2023-04-24 14:11:05 -0600

    Cliente and Server on C

---
+++ Servidor_Cliente/clienteTicTacToe.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main(int argc, char const *argv[]) {
+  int sock = 0, valread;
+  struct sockaddr_in serv_addr;
+  char *hello = "password123";
+  char buffer[1024] = {0};
+
+  // Creating socket
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("\n Socket creation error \n");
+    return -1;
+  }
+
+  memset(&serv_addr, '0', sizeof(serv_addr));
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(PORT);
+
+  // Convert IPv4 and IPv6 addresses from text to binary form
+  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+    printf("\nInvalid address/ Address not supported \n");
+    return -1;
+  }
+
+  // Connect to the server
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    printf("\nConnection Failed \n");
+    return -1;
+  }
+
+  // Send authentication password to the server
+  send(sock, hello, strlen(hello), 0);
+  printf("Password sent\n");
+  printf("Your password is password123\n");
+
+  // Receive response from the server
+  valread = read(sock, buffer, 1024);
+  printf("%s\n", buffer);
+  return 0;
+}
---
+++ Servidor_Cliente/servidorTicTacToe.c
@@ -0,0 +1,79 @@
+//Servidor Autentificación 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+//#include "ticTacToe.h"
+
+#define PORT 8080
+
+int main(int argc, char const *argv[]) {
+  int server_fd, new_socket, valread;
+  struct sockaddr_in address;
+  int opt = 1;
+  int addrlen = sizeof(address);
+  char buffer[1024] = {0};
+  char *hello = "Auth successful";
+
+  // Creating socket file descriptor
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+  }
+  
+  // Forcefully attaching socket to the port 8080
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    perror("setsockopt");
+    exit(EXIT_FAILURE);
+  }
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(PORT);
+
+  // Binding socket
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // Listening to incoming client requests
+  if (listen(server_fd, 3) < 0) {
+    perror("listen");
+    exit(EXIT_FAILURE);
+  }
+
+  // Accepting incoming client requests
+  if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+    perror("accept");
+
+    exit(EXIT_FAILURE);
+  }
+
+
+  // Receiving client message and sending response
+  valread = read(new_socket, buffer, 1024);
+  if (strcmp(buffer, "password123") == 0) {
+    send(new_socket, hello, strlen(hello), 0);
+    printf("Auth successful\n");
+    printf("Password user: password123\n");
+   // ticTacToe();
+
+  } else {
+    printf("Auth failed\n");
+  }
+  return 0;
+
+  if ( send(server_fd, hello, strlen(hello), 0) == -1) {
+		perror("send");
+		exit(1);
+	}
+
+  close(server_fd);  
+   printf("Conexion cerrada\n");
+	return 0;
+
+}
